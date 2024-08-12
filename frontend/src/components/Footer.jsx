3702967f41e35e1185b200528ca4dd6b992e7f1e
mushahidhussain11 <mushahidhussainansari123@gmail.com>
2024-08-12 10:08:54 +0500

    first commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
-			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
+			<div className='flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
 					Built by{" "}
 					<a
